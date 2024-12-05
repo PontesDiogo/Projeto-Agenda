c91c8584f57947c7379a0149abceaef0bd6c9c22
deogo <diogogpontes@gmail.com>
2024-12-05 10:00:29 -0300

    Criando o atualizar e deletar

--- src/main/java/com/trabalho/Agenda/Controllers/CompromissoController.java
+++ src/main/java/com/trabalho/Agenda/Controllers/CompromissoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,9 @@ import com.trabalho.Agenda.DTOs.CompromissoResponse;
 import com.trabalho.Agenda.Services.CompromissoService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @RestController
 @RequestMapping("compromissos")
@@ -37,5 +41,18 @@ public class CompromissoController {
         return ResponseEntity.created(null).body(newCompromisso);
     }
 
-    
+    @PutMapping("{id}")
+    public ResponseEntity<Void> updateCompromisso(
+        @Validated @PathVariable Long id, @RequestBody CompromissoRequest compromisso) {
+
+        service.update(compromisso, id);
+        
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteCompromisso(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/trabalho/Agenda/Services/CompromissoService.java
+++ src/main/java/com/trabalho/Agenda/Services/CompromissoService.java
@@ -16,26 +16,48 @@ import com.trabalho.Agenda.Reposiotry.CompromissoRepository;
 public class CompromissoService {
     @Autowired
     private CompromissoRepository repository;
+
     public List<CompromissoResponse> getAllCompromisso() {
         return repository.findAll().stream().map(c -> CompromissoMapper.toDto(c)).collect(Collectors.toList());
     }
 
     public CompromissoResponse getCompromissoById(Long id) {
         Compromisso compromisso = repository.findById(id).orElseThrow(
-            () -> new RuntimeException("Compromisso não cadastrado"));
+                () -> new RuntimeException("Compromisso não cadastrado"));
         return CompromissoMapper.toDto(compromisso);
     }
 
     // public CompromissoResponse getCompromissoByName(String titulo) {
-    //     Compromisso compromisso = repository.findByTitulo(titulo).orElseThrow(
-    //         () -> new RuntimeException("Compromisso não cadastrado"));
-    //     return CompromissoMapper.toDto(compromisso);        
-        
+    // Compromisso compromisso = repository.findByTitulo(titulo).orElseThrow(
+    // () -> new RuntimeException("Compromisso não cadastrado"));
+    // return CompromissoMapper.toDto(compromisso);
+
     // }
-    
+
     public CompromissoResponse save(CompromissoRequest compromisso) {
         Compromisso compromissoEntity = CompromissoMapper.toEntity(compromisso);
         Compromisso compromissoSaved = repository.save(compromissoEntity);
         return CompromissoMapper.toDto(compromissoSaved);
+    }
+
+    public void update(CompromissoRequest compromisso, Long id) {
+        Compromisso aux = repository.getReferenceById(id);
+
+        aux.setTitulo(compromisso.titulo());
+        aux.setDescricao(compromisso.descricao());
+        aux.setDataInicio(compromisso.dataInicio());
+        aux.setDataFim(compromisso.dataFim());
+        aux.setLocal(compromisso.local());
+
+        repository.save(aux);
+    }
+
+    public void delete(Long id) {
+
+        if (repository.existsById(id)) {
+            repository.deleteById(id);
+        } else {
+            throw new RuntimeException("Compromisso nao cadastrado");
+        }
     }
 }
