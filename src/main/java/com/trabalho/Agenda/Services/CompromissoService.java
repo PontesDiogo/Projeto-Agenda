c1b733d7f530b5f5a3cc3be29a75470debdeeb1d
deogo <diogogpontes@gmail.com>
2024-12-05 09:10:37 -0300

    Criando o service para trazer todos os compromissos da agenda

--- src/main/java/com/trabalho/Agenda/Services/CompromissoService.java
+++ src/main/java/com/trabalho/Agenda/Services/CompromissoService.java
@@ -1,5 +1,20 @@
 package com.trabalho.Agenda.Services;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.trabalho.Agenda.DTOs.CompromissoResponse;
+import com.trabalho.Agenda.Entities.Compromisso;
+import com.trabalho.Agenda.Mappers.CompromissoMapper;
+import com.trabalho.Agenda.Reposiotry.CompromissoRepository;
+
 public class CompromissoService {
+    @Autowired
+    private CompromissoRepository repository;
+    public List<CompromissoResponse> getAllCompromisso() {
+        return repository.findAll().stream().map(c -> CompromissoMapper.toDto(c)).collect(Collectors.toList());
+    }
     
 }
